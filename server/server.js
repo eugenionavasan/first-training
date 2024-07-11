57bc1b7b909b3c2cd8d4c9f62512ca3b752bb2e3
Eugenio Navajo <eugenio.navasan@gmail.com>
2024-07-11 17:40:55 +0200

    working thing

--- server/server.js
+++ server/server.js
@@ -16,39 +16,6 @@ app.use(bodyParser.json());
 const User = db.User;
 
 
-//ROUTES
-
-//GET USERS
-app.get('/users', async (req, res) => {
-  const users = await User.findAll();
-  res.json(users);
-});
-
-//POST USER
-
-app.post('/users', async (req, res) => {
-  const {name, age, email} = req.body;
-  const newUser = await User.create({name, age, email});
-  res.json(newUser);
-});
-
-//DELETE USER
-
-app.delete('/users/:id', async (req, res) => {
-  const id = req.params.id;
-  await User.destroy({ where: { id } });
-  res.sendStatus(204);
-});
-
-//PUT USER
-
-app.put('/users/:id', async (req, res) => {
-  const id = req.params.id;
-  const {name, age, email} = req.body;
-  await User.update({name, age, email}, { where: { id } });
-  const updatedUser = await User.findByPk(id);
-  res.sendStatus(204);
-});
 
 const PORT = process.env.PORT || 3000;
 db.sequelize.sync().then(() => {
